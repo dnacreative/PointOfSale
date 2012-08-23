7a8521918c808b4a904c1cda6801dcb06eba4458
Bill Jameson <hothousevt@gmail.com>
2012-08-23 01:10:14 -0400

    Truncate invn.dat
    invn.dat tends to contain lots of '\x00' garbage at the end

--- extractinv.py
+++ extractinv.py
@@ -10,7 +10,12 @@ invn_file = open('QBPOS/RPRO/INVN.DAT', 'rb')
 invn_contents = invn_file.read()
 invn_file.close()
 
+# the file may be upwards of 75% '\x00' at the end
+invn_contents = re.sub(b'\x00+$', b'', invn_contents)
+
+# this approach probably not ideal since not all entries have all fields;
+# better to just extract each entry and then figure out what to do with it
 # groups, respectively:
 #                 DEPT       VENDOR       DESC1
-item_format = b'\t(\w+) *\x03(\w+)\x00{3}.(.+?)\x00+'
-items = re.findall(item_format, invn_contents)
+#item_format = b'\t(\w+) *\x03(\w+)\x00{3}.(.+?)\x00+'
+#items = re.findall(item_format, invn_contents)
