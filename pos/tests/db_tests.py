94c9f54e6381ff218ce1614aa500dc6d48a3a18a
Bill Jameson <williamjameson@alum.rpi.edu>
2013-12-28 23:34:56 -0500

    Start testing
    Begin writing tests for database

--- pos/tests/db_tests.py
+++ pos/tests/db_tests.py
@@ -1,11 +1,35 @@
 '''
-Unit tests for the database
+Database tests
 '''
 import unittest
 
 from sqlalchemy import create_engine
 
-from pos.db import session, Base, Item, Vendor, Department, UPC, Adjustment
+from pos.db import db_session, Base, Vendor, Department, Item
+
+# creating objects that instantiate the tables queries the DB
+# therefore, only create them as needed rather than immediately on import
+# (this is especially important with Items, which fetch Vendor/Dept IDs)
+def get_test_vendors():
+    return [
+        Vendor('DGS', 'Douglas'),
+        Vendor('PUT', 'Putamayo'),
+        Vendor('ING', 'Ingram')
+    ]
+    
+def get_test_depts():
+    return [
+        Department('PL', 'plush'),
+        Department('MUS', 'music'),
+        Department('FIC', 'fiction')
+    ]
+    
+def get_test_items():
+    return [
+        Item('Snowy Owl', 'Douglas', 5, True, 5.00, 9.99, vendor_code='DGS', dept_code='PL'),
+        Item('Celtic Tides', 'CD', 3, True, 10.00, 19.99, vendor_code='PUT', dept_code='MUS'),
+        Item('Hunger Games', 'Collins/Ingram', 2, True, 4.00, 7.99, vendor_code='ING', dept_code='FIC')
+    ]
 
 class test_item_table(unittest.TestCase):
     def setUp(self):
@@ -23,5 +47,18 @@ class test_item_table(unittest.TestCase):
     def tearDown(self):
         pass
     
+def add_test_data():
+    test_vendors = get_test_vendors()
+    test_depts = get_test_depts()
+    db_session.add_all(test_vendors + test_depts)
+    
+    db_session.commit()
+
+    # add the items separately after the vendors/departments so the IDs will be set correctly
+    test_items = get_test_items()
+    db_session.add_all(test_items)
+    
+    db_session.commit()
+
 if __name__ == '__main__':
     unittest.main()
